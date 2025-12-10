803fbcf3d901509031b0838467f61737fddc2481
Chaerim Han <96909637+cozyrim@users.noreply.github.com>
2025-12-10 21:10:07 +0900

    fix .env

--- apps/frontend/services/socket.js
+++ apps/frontend/services/socket.js
@@ -36,11 +36,7 @@ class SocketService {
           this.cleanup(CLEANUP_REASONS.RECONNECT);
         }
 
-          const socketUrl =
-              process.env.NEXT_PUBLIC_SOCKET_URL ??
-              (typeof window !== "undefined"
-                  ? `${window.location.origin.replace(/^http/, "ws")}`
-                  : "");
+        const socketUrl = process.env.NEXT_PUBLIC_SOCKET_URL;
 
         this.socket = io(socketUrl, {
           ...options,
